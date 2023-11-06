092b3e5f0527de2769abccc9e8e203bdcb2a1bea
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2023-11-06 20:47:30 +0000

    fixed positioning again using grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import ChatFeed from './ChatFeed';
 function App() {
   return (
     <>
+<div className="grid-container">
+
+  <div className="grid-item1">
+
+  <div className='Container4'>
     <div className="Container" >
     
         <CountdownTimer />
@@ -20,31 +25,37 @@ function App() {
           <div className="monitor">
             <div className="turn">
               <ul className="flip6">
-                <li className='text-box'>And that made me crazy</li>
-                <li className='text-box'>Filled with rats</li>
-                <li className='text-box'>A rubber room</li>
-                <li className='text-box'>They put me in a room</li>
-                <li className='text-box'>I was crazy once</li>
-                <li className='text-box'>Crazy?</li>
+                <li className='text-box'>I cba pausing it</li>
+                <li className='text-box'>You don't have time</li>
+                <li className='text-box'>Why you still here?</li>
+                <li className='text-box'>You still here??</li>
+                <li className='text-box'>Its going down</li>
+                <li className='text-box'>Timers going down</li>
               </ul>
             </div>
           </div>
         </div>
     </div>
 
+  </div>
+
     </div>
-      <div className="Container3">
+
+
+
+  </div>
+  <div className="grid-item2">
+
+
+  <div className="Container3">
       <div className="sidebar">
         <ChatFeed />
       </div>
       </div>
 
-      
 
-
-      
-
-  
+  </div>
+</div>
     </>
   )
 }
